220bc3b7704b711999d29f75e324f37feec071f6
Ismail <ismailsmakhus@gmail.com>
2023-04-17 07:19:51 +0700

    search api feature

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,8 +13,50 @@ class ProductController extends Controller
 {
     public function search(Request $request): JsonResponse
     {
+        $sku = (array) $request->query('sku');
+        $name = (array) $request->query('name');
+        $priceStart = (int) $request->query('price_start');
+        $priceEnd = (int) $request->query('price_end');
+        $stockStart = (int) $request->query('stock_start');
+        $stockEnd = (int) $request->query('stock_end');
+        $categoryId = (array) $request->query('category_id');
+        $categoryName = (array) $request->query('category_name');
+
+        $query = Product::with('category');
+        if (count($sku)) {
+            $query = $query->whereIn('sku', $sku);
+        }
+
+        if ($priceStart) {
+            $query = $query->where('price', '>=', $priceStart);
+        }
+
+        if ($priceEnd) {
+            $query = $query->where('price', '<=', $priceEnd);
+        }
+
+        if ($stockStart) {
+            $query = $query->where('stock', '>=', $stockStart);
+        }
+
+        if ($stockEnd) {
+            $query = $query->where('stock', '<=', $stockEnd);
+        }
+
+        if (count($categoryId)) {
+            $query = $query->whereIn('category_id', $categoryId);
+        }
+
+        $pageSize = 10;
+        $products = $query->paginate($pageSize);
+
         return response()->json([
-            'data' => ProductResource::collection(Product::with('category')->get()),
+            'data' => ProductResource::collection($products),
+            'paging' => [
+                'size' => $pageSize,
+                'total' => $products->lastPage(),
+                'current' => $products->currentPage(),
+            ],
         ]);
     }
 }
